393f2efa6598ab5427c5627fe3bdb75d27732fef
Clint Monzon <clint@ativistar.com>
2018-07-26 12:35:52 +0400

    Files updated

--- index.php
+++ index.php
@@ -2,8 +2,37 @@
 <html>
 	<head>
 		<title>Zaks Burger - Your masterpiece burger!</title>
+
+		<link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
+
+		<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+		<link href="style.css" rel="stylesheet" />
 	</head>
 	<body>
-		
+
+		<div class="mainContainer">
+			<div class="container flexiContainer">
+				<section class="main">
+					<img src="logo.svg" class="logo" />
+					<img src="burger.png" class="burger" />
+					<h1>Your Masterpiece Burger</h1>
+					<h2>Opening on 2036!</h2>
+					<p>You're probably here because of Sapaad.Online.<br />If you want to go back, <a href="https://www.sapaad.com/sapaad.online/?ref=zaksburger" target="_blank">click here</a> or if you want to know about us, <a href="http://zaksadventure.com/?ref=zaksburger" target="_blank">click here</a>.</p>
+				</section>
+			</div>
+
+			<figure class="burger-stuff">
+				<img src="lettuce.png" class="lettuce" />
+				<img src="tomato.png" class="tomato" />
+				<img src="onion.png" class="onion" />
+				<img src="cheese.png" class="cheese" />
+			</figure>
+		</div>
+
+		<script
+		  src="https://code.jquery.com/jquery-3.3.1.min.js"
+		  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+		  crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 	</body>
 </html>
