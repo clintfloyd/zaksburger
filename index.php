fea5e031422263a80f648930c90646883658bc54
Clint Monzon <clint@ativistar.com>
2018-07-26 14:45:58 +0400

    viewport added

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 		<title>Zaks Burger - The Home of Chargrilled Patties!</title>
 
 		<link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
